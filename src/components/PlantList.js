561aadb1b8a3c80313a6b21ff88a2b0cac23f5a2
Amanda <ajnikrant@gmail.com>
2021-05-05 09:24:48 -0400

    Done.

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantArray}) {
+
+  const renderPlants = plantArray.map((plant) => <PlantCard key={plant.id} plant={plant} />)
+
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{renderPlants}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantArray, setPlantArray] = useState([])
+  const [search, setSearch] = useState("")
+
+
+  useEffect(()=> {
+    fetch('http://localhost:6001/plants')
+    .then(r => r.json())
+    .then(setPlantArray)
+  }, [])
+
+  function onAddPlant(newPlant){
+    setPlantArray([...plantArray, newPlant])
+  }
+
+  const filteredArray = plantArray.filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm sendPlantUp={onAddPlant}/>
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plantArray={filteredArray}/>
     </main>
   );
 }
